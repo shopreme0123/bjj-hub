f731af7f6ea4b92509d7490cd6be752caea5cd85
Shogo Nakajima <shogonakajima@shougonoMacBook-Air.local>
2026-01-03 21:49:12 +0900

    HP追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,9 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;600;700;900&family=Bebas+Neue&display=swap" rel="stylesheet" />
         <link rel="apple-touch-icon" href="/icon-192.png" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
       </head>
